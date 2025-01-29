103468561a94baafa7fc1633417fc4ae74c6776a
sakhaglobal <fizaasayyed7@gmail.com>
2025-01-29 15:21:07 +0530

    profile card in place

--- testimonial-card/js/index.js
+++ testimonial-card/js/index.js
@@ -1,2 +1,10 @@
 // Write custom JavaScript here.
 // You may ignore this file and delete if if JavaScript is not required for your challenge.
+// let contentElement = document.getElementById('testimonial-content');
+// const fullText = contentElement.dataset.fullText;
+// const maxLength = 50;
+
+// if (content.length > maxLength) {
+//     console.log('Hello from inside', content);
+//     contentElement.textContent = fullText.slice(0, maxLength) + '...';
+// }
